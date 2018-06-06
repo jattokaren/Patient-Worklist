6f1cbad579215c7349750360c9ad60c78883f8b2
Joe Attok <jattokaren@gmail.com>
2018-06-06 09:52:58 -0700

    Organizing and cleaning up a working Notes page

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'encounters#index'
 
-
-  resources :encounters do
+  resources :encounters do #nested resources
   	resources :notes 	
   end
 
