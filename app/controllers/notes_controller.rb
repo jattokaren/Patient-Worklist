39d6cb72f9f654fdad1ed3da93dd09ee131a67de
Joe Attok <jattokaren@gmail.com>
2018-06-10 22:29:53 -0700

    Limited Routes, Updated Form code (partials), Tried to Edit, Update, Delete Notes

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -6,6 +6,7 @@ class NotesController < ApplicationController
   end
 
   def show
+  	#link_to("Show Note page", encounter_note_path(@encounter_id, note))
   	@encounter_id = params[:encounter_id]
   	@note_id = params[:id]
   	@note = Note.find(params[:id])
@@ -21,6 +22,31 @@ class NotesController < ApplicationController
   	note.encounter_id = params[:encounter_id]
   	note.save
   	redirect_to encounter_notes_path(params[:encounter_id])
+  end
+
+  def edit
+  	#NOT WORKING maybe
+  	@encounter_id = params[:encounter_id]
+  	@note_id = params[:id]
+  	@note = Note.find(params[:id])
+  end
+
+  def update
+  	#NOT WORKING
+  	@encounter_id = params[:encounter_id]
+  	@note_id = params[:id]
+  	@note = Note.find(params[:id])
+  	@note.update_attributes(note_params)
+  	redirect_to encounter_notes_path(params[:encounter_id])
+  end
+
+  def destory
+  	#NOT WORKING
+  	@encounter_id = params[:encounter_id]
+  	@note_id = params[:id]
+  	@note = Note.find(params[:id])
+	@note.destroy
+	redirect_to encounter_notes_path(params[:encounter_id])
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'encounters#index'
 
-  resources :encounters do #nested resources
+  # nested resources
+  resources :encounters, only: [:index, :show] do 
   	resources :notes 	
   end
 
