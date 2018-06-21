4efd10502434cc3660230fd85252b56e85459bb4
Joe Attok <jattokaren@gmail.com>
2018-06-20 22:56:48 -0700

    Heroku sprocket vulnerability fix,, Remove partial forms, Tried to Edit, Update, Delete Notes

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -14,36 +14,41 @@ class NotesController < ApplicationController
 
   def new
   	@encounter_id = params[:encounter_id]
+  	#@encounter_patient_account_number = params[:patient_account_number]
   	@note = Note.new
   end
 
   def create
+  	p "In the create method!!!!"
   	note = Note.new(note_params)
+  	p note
   	note.encounter_id = params[:encounter_id]
+  	p note.encounter_id
+  	#NOT WORKING Pass PAN from Enc to Note
+  	#note.patient_account_number = params[:patient_account_number]
+  	#p note.patient_account_number #nil?? why?
   	note.save
   	redirect_to encounter_notes_path(params[:encounter_id])
   end
 
-  def edit
-  	#NOT WORKING maybe
-  	@encounter_id = params[:encounter_id]
-  	@note_id = params[:id]
-  	@note = Note.find(params[:id])
-  end
+    def edit  #NOT WORKING maybe 
+  @encounter_id = params[:encounter_id] 
+  @note_id = params[:id] 
+  @note = Note.find(params[:id]) 
+  end 
 
-  def update
-  	#NOT WORKING
-  	@encounter_id = params[:encounter_id]
-  	@note_id = params[:id]
-  	@note = Note.find(params[:id])
-  	@note.update_attributes(note_params)
-  	redirect_to encounter_notes_path(params[:encounter_id])
-  end
+  def update   #NOT WORKING WEll 
+    @encounter_id = params[:encounter_id] 
+  #	@note_id = params[:id] 
+    @note = Note.find(params[:id]) 
+    @note.update_attributes(note_params) 
+  redirect_to encounter_notes_path 
+  end 
 
-  def destory
-  	#NOT WORKING
+  def destory  	#NOT WORKING well?
+  	#<td><%= link_to 'Delete Note', encounter_note_path, method: :delete %></td>
   	@encounter_id = params[:encounter_id]
-  	@note_id = params[:id]
+  	#@note_id = params[:id]
   	@note = Note.find(params[:id])
 	@note.destroy
 	redirect_to encounter_notes_path(params[:encounter_id])
@@ -53,6 +58,7 @@ class NotesController < ApplicationController
 
   def note_params
   	params.require(:note).permit(:content, :staff_initials)
+  	#params.require(:note).permit(:patient_account_number, :content, :staff_initials)
   end
 
 
