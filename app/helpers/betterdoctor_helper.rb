d13adb1ec1126348f50fd354d4cba4e658ce9bc7
Joe Attok <jattokaren@gmail.com>
2018-09-25 21:23:17 -0700

    Declare env var

--- app/helpers/betterdoctor_helper.rb
+++ app/helpers/betterdoctor_helper.rb
@@ -2,7 +2,7 @@ require 'net/http'
 
 module BetterdoctorHelper
  def self.get_doctors
-    url = URI.parse('https://api.betterdoctor.com/2016-03-01/doctors?location=az-phoenix&sort=best-match-desc&skip=0&limit=10&user_key=' + Rails.application.secrets.bd_api)   
+    url = URI.parse('https://api.betterdoctor.com/2016-03-01/doctors?location=az-phoenix&sort=best-match-desc&skip=0&limit=10&user_key=' + ENV['BD_API'])   
     req = Net::HTTP::Get.new(url.to_s)
     req['Accept'] = 'application/json'
     #req.add_field('Authorization', 'SHAREDKEY 1197:n1ndBzCNh+hW7kbVueswEsCGNiDd1yS6U4MKk3kOe14=')
